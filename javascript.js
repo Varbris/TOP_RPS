ecb381d99da3ef2e00c0f78cf9112c4e7f19324c
Varbris <92345143+Varbris@users.noreply.github.com>
2025-01-30 08:33:01 +0700

    Fix computer selection is produce same rps
    the problem is the computerselection variable in
    playGame function doesnt included to
    button event listener. it makes the computer
    selection always same even when we click the button

--- javascript.js
+++ javascript.js
@@ -33,7 +33,7 @@ function toInsensitive(text) {
 function playRound(humanChoise, computerChoise) {
   const winner = checkWinner(humanChoise, computerChoise);
   if (winner === 0) {
-    console.log("Its a Draw");
+    return 0;
   } else if (winner === 1) {
     return humanScore++;
   } else if (winner === 2) {
@@ -43,27 +43,18 @@ function playRound(humanChoise, computerChoise) {
 
 function checkWinner(humanChoise, computerChoise) {
   if (humanChoise === computerChoise) {
-    console.log(
-      "Human: " + humanChoise + " vs " + "Computer: " + computerChoise
-    );
-    return 0;
+    console.log("its a tie");
   } else if (
     (humanChoise === "paper" && computerChoise === "rock") ||
     (humanChoise === "rock" && computerChoise === "scissor") ||
     (humanChoise === "scissor" && computerChoise === "paper")
   ) {
-    console.log(
-      "Human: " + humanChoise + " vs " + "Computer: " + computerChoise
-    );
     return 1;
   } else if (
     (computerChoise === "paper" && humanChoise === "rock") ||
     (computerChoise === "rock" && humanChoise === "scissor") ||
     (computerChoise === "scissor" && humanChoise === "paper")
   ) {
-    console.log(
-      "Human: " + humanChoise + " vs " + "Computer: " + computerChoise
-    );
     return 2;
   }
 }
@@ -99,30 +90,41 @@ function playGame() {
   showGame();
   const buttonList = document.querySelectorAll("button");
   let humanSelection;
-  const computerSelection = toInsensitive(getComputerChoise());
   const div = document.querySelector(".div");
   const human = document.createElement("h2");
   const computer = document.createElement("h2");
 
   for (const button of buttonList) {
     button.addEventListener("click", function (e) {
+      const computerSelection = toInsensitive(getComputerChoise());
       humanSelection = getHumanChoise(e.target.value);
       if (humanSelection === 0) {
         console.log("Input Invalid");
       } else {
         playRound(humanSelection, computerSelection);
+        console.log(humanSelection);
+        console.log("computer:", computerSelection);
       }
       human.innerText = "Human Score: " + humanScore;
       computer.innerText = "Computer Score: " + computerScore;
       div.appendChild(human);
       div.appendChild(computer);
+
+      // checkFinalScore(humanScore, computerScore);
     });
   }
 }
 
 function checkFinalScore(human, computer) {
-  if (human === 5) {
+  if (human === 0 && computer === 0) {
+    console.log("Game Start!");
+  } else if (human === 5) {
     console.log("Humanity Win! final score: " + human);
+    const div = document.querySelector(".div");
+    div.innerHTML = "";
+    const announcement = document.createElement("h1");
+    announcement.innerText = "Humanity Win! final score: " + human;
+    div.appendChild(announcement);
   } else if (computer === 5) {
     console.log("Computer Win! final score: " + computer);
   } else {
@@ -135,4 +137,3 @@ let computerScore = 0;
 
 const playButton = document.querySelector("#play");
 playButton.addEventListener("click", () => playGame());
-checkFinalScore(humanScore, computerScore);
